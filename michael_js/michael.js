8f1ff0abafe892a2bad88cb4326463d187c29ecd
Gurol Bilgin <gurolbilgin@gmail.com>
2021-07-07 23:10:13 -0600

    adds michael's questions' answers

---
+++ michael_js/michael.js
@@ -0,0 +1,98 @@
+
+// Write a function that returns the string "something" joined with a space " " and the given argument a
+
+
+// ----------- solution 1 
+// function addToStart (a) {
+//     return "something" + " " + a
+// }
+
+// console.log(addToStart("bob Jane"));
+
+
+// ----------- solution 2
+// let x = 'something';
+// let y = " ";
+
+// function addToStart (a) {
+//     return x.concat(y,a)
+// }
+
+// console.log(addToStart("bob Jane"));
+
+//----------- solution 3
+
+// let x = 'something';
+// function addToStart (a) {
+//     return x.concat(" ", a)
+// }
+// console.log(addToStart("bob Jane"));
+
+// ------------------------------------------------------------------------
+//******* Question *********   ????????????? michael's questions
+// here is a simple challenge:
+// write a function
+// input: string => "21/06/2019"
+// output: string => "2019-06-21"
+
+// lets make it more generic and complex.
+// input: string => "21/06/2019" or "21.06.2019" or "21 06 2019" etc.
+// output: string => "2019-06-21"
+
+// let correctEntry = true;
+
+// while (correctEntry) {
+//     var input = window.prompt("Enter your date using DD/MM/YYYY, DD.MM.YYYY or DD MM YYYY format to organize: ")
+//     console.log(input);
+//     if (input.length == 10);
+//     correctEntry = false;
+       
+// }
+
+// let output  = function (inpt) {
+//     let char = /[ ./]/g;
+//     inpt = inpt.replace(char, " ").split(' ');
+//     inpt = [inpt[2], inpt[1], inpt[0]].join('-');
+//     return inpt;
+// }
+
+// console.log(output(input));
+
+// ------------------------------------------
+//------ Question Michael2 
+
+// hi there.  hope you are doing fine. here is a simple challenge (real time problem):
+// get an input from user (use prompt) until you get a valid number between 0 and 100.
+
+// let entry = true;
+
+// while (entry) {
+//     const number = prompt("Please enter a number between 0 and 100: ");
+    
+//     if ( (Number(number)) &&  1 <= number.length <= 3 ) 
+//     entry = false;
+//     console.log(number);
+
+
+// }
+
+// there are two other methods to convert strings to numbers:
+// parseInt(string, 10) ==> second parametre is radix which is base 10 in our condition
+// parseFloat("3.14randomstuff") returns 3.14 
+
+
+//***********next step (we will do this together sooner): use text input in html and warn user if the number is not valid, by changing its border color to red. until you get a valid number.
+
+// ---------------------
+
+//-------------------- Michael Question 3 
+// Create an application which selects a name randomly from a list. Then we can use it whenever we need
+
+const starWarsCharachterList = ["Darth", "Kenobi", "Sith", "Mandalorian", "C-3PO", "Anakin", "Han", "Yoda"];
+
+let lengthList = starWarsCharachterList.length;
+
+let dice = Math.floor(Math.random() * lengthList);
+let randomName = starWarsCharachterList[dice];
+
+console.log(randomName);
