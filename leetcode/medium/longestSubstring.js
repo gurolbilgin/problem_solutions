b1428d36416f42a72bd95523c2a50551f45ee64a
Gurol Bilgin <gurolbilgin@gmail.com>
2021-09-19 22:53:35 -0600

    adds unconfirmed solution

--- leetcode/medium/longestSubstring.js
+++ leetcode/medium/longestSubstring.js
@@ -29,27 +29,49 @@
 
 const lengthOfLongestSubstring = (s) => {
   let arr = [...s];
-  let stock = [];
-  let subArr = [];
-  let blankCount = 0;
+  // let stock = [];
+  // let subArr = [];
+  // let blankCount = 0;
 
-  arr.map((e) => {
-    stock.indexOf(e) === -1 ? stock.push(e) : stock.push("") && stock.push(e);
-    console.log(stock);
-  });
+  // arr.map((e) => {
+  //   stock.indexOf(e) === -1 ? stock.push(e) : stock.push("") && stock.push(e);
+  //   console.log(stock);
+  // });
 
-  // console.log(stock);
-  // console.log(stock.indexOf(""));
+  // // console.log(stock);
+  // // console.log(stock.indexOf(""));
 
-  stock.map((e) => (e !== "" ? null : blankCount++));
-  stock.map((e) => (e !== "" {
-    if (subArr.length === 0)  subArr = stock.splice(0, stock.indexOf(e) + 1)
-     
+  // stock.map((e) => (e !== "" ? null : blankCount++));
+  // stock.map((e) => {
+  //   if (e !== "") {
+  //     subArr = subArr.splice(0, subArr.length);
+  //     subArr.push(e);
+  //     console.log("subArr", subArr);
+  //   }
+  // });
+  let count = 0;
+
+  let begining = 0;
+  let ending = 0;
+  let set = new Set();
+
+  while (begining < s.length && ending < s.length) {
+    let char = s.charAt(ending);
+    console.log(char);
+    if (!set.has(char)) {
+      set.add(char);
+      ending++;
+      count = Math.max(count, ending - begining);
+    }
+    // else {
+    //   set.delete(s.charAt(begining))
+    // }
   }
-  ));
+  console.log(count);
+  console.log(set);
 
-  console.log("subArr", subArr);
-  console.log(blankCount);
+  // console.log("subArr", subArr);
+  // console.log(blankCount);
 
   //   console.log(stock.length);
   //   console.log(stock.filter((e) => e == "").length);
