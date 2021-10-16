46356010a7f79f5bbe9379b1f0a0a485a4bb02e2
Gurol Bilgin <gurolbilgin@gmail.com>
2021-10-16 07:27:35 -0600

    adds new problems

--- leetcode/medium/zigzag_conversion.js
+++ leetcode/medium/zigzag_conversion.js
@@ -1,14 +1,51 @@
-/*
- * @param {string} s
- * @param {number} numRows
- * @return {string}
- */
+// The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+// P   A   H   N
+// A P L S I I G
+// Y   I   R
+// And then read line by line: "PAHNAPLSIIGYIR"
+
+// Write the code that will take a string and make this conversion given a number of rows:
+
+// string convert(string s, int numRows);
+
+// Example 1:
+
+// Input: s = "PAYPALISHIRING", numRows = 3
+// Output: "PAHNAPLSIIGYIR"
+// Example 2:
+
+// Input: s = "PAYPALISHIRING", numRows = 4
+// Output: "PINALSIGYAHRPI"
+// Explanation:
+// P     I    N
+// A   L S  I G
+// Y A   H R
+// P     I
+// Example 3:
+
+// Input: s = "A", numRows = 1
+// Output: "A"
+
+// Constraints:
+
+// 1 <= s.length <= 1000
+// s consists of English letters (lower-case and upper-case), ',' and '.'.
+// 1 <= numRows <= 1000
 
 const convert = function (s, numRows) {
-  const charCount = 0
-  const arrayCount = 0
-  for (let i in s) {
-    charCount++
-  }
-  
+  let arrayCt = 0;
+  let blockCt = 1;
+  let blockCharCt = 0;
+  blockCt += numRows - 2;
+  // console.log({ blockCt });
+  blockCharCt = numRows + blockCt - 1;
+  // console.log({ blockCharCt });
+  totalBlckCt = Math.ceil(s.length / blockCharCt);
+  // console.log({ totalBlckCt });
 };
+
+// console.log(convert("PAYPALISHIRING", 1));
+console.log(convert("PAYPALISHIRING", 3));
+console.log(convert("PAYPALISHIRING", 4));
+console.log(convert("A", 1));
